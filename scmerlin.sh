e12a13c6ba1cfc0313da41bb8b4287eaa930db5a
Martinski4GitHub <119833648+Martinski4GitHub@users.noreply.github.com>
2026-06-24 07:00:27 -0700

    Fix for Missing "cookie" Helper Functions
    Fix for compatibility issue with latest AsusWRT6 routers, such as the GT-BE19000AI, running 3006.102.8 F/W version, where the webpage generates errors due to missing "cookie" helper functions previously defined in the 'state.js' file (Ported from Unbound Stats WebUI ASP fix supplied by @ExtremeFiretop).

--- scmerlin.sh
+++ scmerlin.sh
@@ -12,7 +12,7 @@
 ## Forked from: https://github.com/jackyaz/scMerlin ##
 ##                                                  ##
 ######################################################
-# Last Modified: 2026-Apr-26
+# Last Modified: 2026-Jun-24
 #-----------------------------------------------------
 
 ##########       Shellcheck directives     ###########
@@ -34,7 +34,7 @@ readonly SCRIPT_NAME="scMerlin"
 readonly SCRIPT_NAME_LOWER="$(echo "$SCRIPT_NAME" | tr 'A-Z' 'a-z' | sed 's/d//')"
 readonly SCM_VERSION="v2.5.50"
 readonly SCRIPT_VERSION="v2.5.50"
-readonly SCRIPT_VERSTAG="26042609"
+readonly SCRIPT_VERSTAG="26062400"
 SCRIPT_BRANCH="develop"
 SCRIPT_REPO="https://raw.githubusercontent.com/AMTM-OSR/$SCRIPT_NAME/$SCRIPT_BRANCH"
 readonly SCRIPT_DIR="/jffs/addons/$SCRIPT_NAME_LOWER.d"
